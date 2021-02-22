3a060615d0b12d5a3834fad4a7e1eed20e54dfbc
André Martins <andremartinsdsilva@gmail.com>
2021-02-22 14:28:55 -0300

    removendo rotas em app.js

--- src/app.js
+++ src/app.js
@@ -2,34 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
+const index = require("./routes/index");
+const product = require("./routes/product");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const route = router.get("/", (req, res, next) => {
-  res.status(200).send({
-    title: "Nodejs API",
-    version: "",
-  });
-});
 
-const create = router.post("/product/", (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const update = router.put("/product/:id", (req, res, next) => {
-  const { id } = req.params;
-  res.status(201).send(`Parametro enviado ${id}`);
-});
-
-const del = router.delete("/product/:id", (req, res, next) => {
-  const { id } = req.params;
-  res.status(200).send({
-    id,
-    title: "qualquer coisa",
-  });
-});
-
-app.use("/", route);
-app.use("/product", create);
-app.use("/product", update);
+app.use("/", index);
+app.use("/product", product);
 
 module.exports = app;
