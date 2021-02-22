6036bc9c1ae7c95f4674b330e8457c19fbe82fdb
André Martins <andremartinsdsilva@gmail.com>
2021-02-22 13:27:39 -0300

    rotas e body-passer

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const route = router.get("/", (req, res, next) => {
   res.status(200).send({
     title: "Nodejs API",
     version: "",
   });
 });
+
+const create = router.post('/', (req, res, next)=>{
+  res.status(201).send(req.body);
+});
+
 app.use("/", route);
 
 module.exports = app;
