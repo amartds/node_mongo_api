a501da26f0f186cb78d7cc185a7d7fb703f85f10
André Martins <andremartinsdsilva@gmail.com>
2021-03-09 00:30:11 -0300

    add database connection

---
+++ src/database/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+class Database {
+  constructor() {
+    this.mongo();
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MOGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+  }
+}
+module.exports = new Database();
