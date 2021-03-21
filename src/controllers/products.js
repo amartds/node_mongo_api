f2f3d838fadfb351e8807af5e74d748dcf08a741
André Martins <andremartinsdsilva@gmail.com>
2021-03-21 20:43:36 -0300

    add methods crud in product

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,27 +1,76 @@
-const Product = require('../models/Product')
+const Product = require("../models/Product");
 
-exports.post = ("/", (req, res, next) => {
-  let product = new Product(req.body);
-  product.save().then(s => {
-    res.status(201).send({
-      "message":"success"
-    });
-  }).catch(e=>{
-    res.status(400).send({
-      "message":"error"
+exports.get =
+  ("products/",
+  (req, res, next) => {
+    Product.find({ active: true }, "title slug price")
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: "nada encontrado" });
+      });
+  });
+
+exports.getBySlug = (req, res, next) => {
+  Product.findOne(
+    { active: true, slug: req.params.slug },
+    "title slug price description"
+  )
+    .then((data) => {
+      res.status(200).send(data);
     })
-  });
-});
+    .catch((err) => {
+      res.status(400).send({
+        message: "not found",
+      });
+    });
+};
 
-exports.put = ("/:id", (req, res, next) => {
-  const { id } = req.params;
-  res.status(201).send(`Parametro enviado ${id}`);
-});
+exports.getById = (req, res, next) => {
+  Product.findOne(
+    {_id: req.params.id })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: "not found",
+      });
+    });
+};
 
-exports.delete = ("/:id", (req, res, next) => {
-  const { id } = req.params;
-  res.status(200).send({
-    id,
-    title: "qualquer coisa",
+exports.post =
+  ("/",
+  (req, res, next) => {
+    let product = new Product(req.body);
+    product
+      .save()
+      .then((s) => {
+        res.status(201).send({
+          message: "success",
+        });
+      })
+      .catch((e) => {
+        res.status(400).send({
+          message: "error",
+        });
+      });
   });
-});
+
+exports.put =
+  ("/:id",
+  (req, res, next) => {
+    const { id } = req.params;
+    res.status(201).send(`Parametro enviado ${id}`);
+  });
+
+exports.delete =
+  ("/:id",
+  (req, res, next) => {
+    const { id } = req.params;
+    res.status(200).send({
+      id,
+      title: "qualquer coisa",
+    });
+  });
