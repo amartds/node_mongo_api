dd782c289979ba08a66c7e514924c75e446e7ce0
André Martins <andremartinsdsilva@gmail.com>
2021-04-01 00:41:28 -0300

    add customer

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const app = express();
 const router = express.Router();
 const index = require("./routes/index");
 const product = require("./routes/product");
+const customer = require("./routes/customer");
+const order = require("./routes/order");
 require('./database/index');
 
 
@@ -14,5 +16,7 @@ app.use(bodyParser.json());
 app.use("/", index);
 app.use("/product", product);
 app.use("/products", product);
+app.use("/customers", customer);
+app.use("/orders", order);
 
 module.exports = app;
